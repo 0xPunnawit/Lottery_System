31fd4dd7e4327c9039bcf048c310567cd233f977
0xPunnawit <0xPunnawit.rod@gmail.com>
2025-10-09 00:53:02 +0700

    lottery

--- src/main/java/com/punnawit/Lottery_System/repository/UserTicketRepository.java
+++ src/main/java/com/punnawit/Lottery_System/repository/UserTicketRepository.java
@@ -1,9 +1,14 @@
 package com.punnawit.Lottery_System.repository;
 
 import com.punnawit.Lottery_System.entity.UserTicket;
+import com.punnawit.Lottery_System.entity.Users;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface UserTicketRepository extends JpaRepository<UserTicket, Long> {
+
+    List<UserTicket> findByUsers(Users user);
 }
---
+++ src/main/java/com/punnawit/Lottery_System/service/PaymentService.java
@@ -0,0 +1,14 @@
+package com.punnawit.Lottery_System.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class PaymentService {
+
+    // Simulate payment
+    public boolean processPayment(String paymentDetails) {
+
+        // TODO: Can connect to actual payment API later
+        return true;  // คืนค่าการชำระเงินสำเร็จ
+    }
+}
